12bb91a38a9bad2f12967849bab3c2051034dd0b
vasyas <vasyl@stashuk.com>
2021-01-25 13:44:49 +0200

    textarea component

---
+++ src/CheckBox.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import {useRef} from "react"
+import {Constraint, Field} from "@use-ui/hooks"
+import {FormGroup, FormGroupProps} from "./FormGroup"
+
+interface Props extends Partial<Constraint>, FormGroupProps {
+  field: Field
+  autoFocus?: boolean
+  disabled?: boolean
+  right?: any
+}
+
+export const CheckBox = ({field, label, autoFocus, disabled, right, ...other}: Props) => {
+  const ref = useRef<HTMLInputElement>()
+
+  field.setFieldElement({
+    constraint: other,
+    type: "boolean",
+    focus: () => ref.current.focus(),
+    blur: () => ref.current.blur(),
+  })
+
+  return (
+    <FormGroup label={label} invalidFeedback={field.getError()}>
+      <input
+        type="checkbox"
+        className="form-control"
+        onChange={e => field.setValue("" + e.target.checked)}
+        onBlur={field.onBlur}
+        onFocus={field.onFocus}
+        ref={ref}
+        autoFocus={autoFocus}
+        disabled={disabled}
+        checked={field.getValue() == "true"}
+      />
+      {right}
+    </FormGroup>
+  )
+}
--- src/TextArea.tsx
+++ src/TextArea.tsx
@@ -1,37 +1,54 @@
 import * as React from "react"
 import {useRef} from "react"
-import {Constraint, Field} from "@use-ui/hooks"
+import {Constraint, Field, FieldTypeName} from "@use-ui/hooks"
 import {FormGroup, FormGroupProps} from "./FormGroup"
 
 interface Props extends Partial<Constraint>, FormGroupProps {
   field: Field
+  type?: FieldTypeName
   autoFocus?: boolean
   disabled?: boolean
   right?: any
+  placeholder?: string
+  rows?: number
+  cols?: number
 }
 
-export const Checkbox = ({field, label, autoFocus, disabled, right, ...other}: Props) => {
-  const ref = useRef<HTMLInputElement>()
+export const TextArea = ({
+  field,
+  type,
+  label,
+  autoFocus,
+  disabled,
+  right,
+  placeholder,
+  rows,
+  cols,
+  ...other
+}: Props) => {
+  const ref = useRef<HTMLTextAreaElement>()
 
   field.setFieldElement({
     constraint: other,
-    type: "boolean",
+    type,
     focus: () => ref.current.focus(),
     blur: () => ref.current.blur(),
   })
 
   return (
     <FormGroup label={label} invalidFeedback={field.getError()}>
-      <input
-        type="checkbox"
+      <textarea
         className="form-control"
-        onChange={e => field.setValue("" + e.target.checked)}
+        value={field.getValue()}
+        onChange={e => field.setValue(e.target.value)}
         onBlur={field.onBlur}
         onFocus={field.onFocus}
         ref={ref}
         autoFocus={autoFocus}
         disabled={disabled}
-        checked={field.getValue() == "true"}
+        placeholder={placeholder}
+        rows={rows}
+        cols={cols}
       />
       {right}
     </FormGroup>
